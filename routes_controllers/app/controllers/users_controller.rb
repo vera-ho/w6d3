43eff17f5016f6f58d236cf900e120b68795d8b9
David-Allen <davida.asencio@gmail.com>
2022-05-04 17:01:37 -0400

    dave pt 2

--- routes_controllers/app/controllers/users_controller.rb
+++ routes_controllers/app/controllers/users_controller.rb
@@ -7,10 +7,37 @@ class UsersController < ApplicationController
     end
 
     def create
-        render json: params 
+        user = User.new(params.require(:user).permit(:name, :email))
+        if user.save
+            render json: user
+        else
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end
     end
 
     def show
-        render json: params[:favorite_pizza]
+        user = User.find(params[:id])
+        render json: user
+    end
+    
+
+
+    def update
+        user = User.find(params[:id])
+        if user.update(params.require(:user).permit(:name, :email))
+            render json: user
+        else
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    def destroy 
+        user = User.find(params[:id])
+
+        if user.destroy
+            render json: user
+        else
+            render json: 'Cant delete user'
+        end
     end
 end
