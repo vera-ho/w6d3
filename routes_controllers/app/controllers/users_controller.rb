f5f6d705c1741fed0410cc1bc73c9367dfea2fc9
David-Allen <davida.asencio@gmail.com>
2022-05-04 15:19:54 -0400

    from dave

--- routes_controllers/app/controllers/users_controller.rb
+++ routes_controllers/app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
-class UsersControllers < ApplicationController
+class UsersController < ApplicationController
 
-    
+    def index 
+        render plain: 'Dave and veras first rails request'
+    end
 
+    def create
+        render json: params 
+    end
+
+    def show
+        render json: params[:favorite_pizza]
+    end
 end
