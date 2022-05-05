c99f32a37961a214fcd1250bc1d1f3553cda054d
Vera Ho <vh71886@gmail.com>
2022-05-05 10:02:45 -0400

    added timestamps to artworks table

--- art_share/db/schema.rb
+++ art_share/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_05_135217) do
+ActiveRecord::Schema.define(version: 2022_05_05_135835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 2022_05_05_135217) do
     t.string "title", null: false
     t.string "image_url", null: false
     t.integer "artist_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["artist_id", "title"], name: "index_artworks_on_artist_id_and_title", unique: true
     t.index ["artist_id"], name: "index_artworks_on_artist_id"
   end
