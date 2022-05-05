a828fb8f66c4d82e16b74c998fdb0967b4be0d84
Vera Ho <vh71886@gmail.com>
2022-05-05 08:54:16 -0400

    added #user_params

--- art_share/config/routes.rb
+++ art_share/config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users
+  # resources :users
 
-  # get 'users/:id', to: 'users#show'
-  # get 'users/', to: 'users#index'
-  # get 'users/new', to: 'users#new'
-  # post 'users/', to: 'users#create'
-  # get 'users/:id/edit', to: 'users#edit'
-  # put 'users/:id', to: 'users#update'
-  # patch 'users/:id', to: 'users#update'
-  # delete 'users/:id', to: 'users#destroy'
+  get 'users/:id', to: 'users#show'
+  get 'users/', to: 'users#index'
+  get 'users/new', to: 'users#new'
+  post 'users/', to: 'users#create'
+  get 'users/:id/edit', to: 'users#edit'
+  put 'users/:id', to: 'users#update'
+  patch 'users/:id', to: 'users#update'
+  delete 'users/:id', to: 'users#destroy'
 end
