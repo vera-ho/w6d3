dc89368efc2b96d0571a3d777b5fcb8812bfd6e0
Vera Ho <vh71886@gmail.com>
2022-05-05 10:22:46 -0400

    wrote associations for artwork share

--- art_share/app/models/user.rb
+++ art_share/app/models/user.rb
@@ -5,7 +5,13 @@ class User < ApplicationRecord
         primary_key: :id,
         foreign_key: :artist_id,
         class_name: :Artwork
-
     
+    has_many :shared_artworks,
+        primary_key: :id,
+        foreign_key: :artwork_id,
+        class_name: :ArtworkShare
 
+    has_many :shared_art,
+        through: :shared_artworks,
+        source: :artwork
 end
